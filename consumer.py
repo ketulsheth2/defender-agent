85f7fb2d1eed1febd285efb461a2f4020bfc5cc5
ketulsheth2 <ketulsheth2@gmail.com>
2019-03-04 18:55:49 -0800

    Store dataset to csv from kafka consumer

--- consumer.py
+++ consumer.py
@@ -1,8 +1,24 @@
 from kafka import KafkaConsumer
 import json
+import csv
+
+row = ['listening_tcp_ports', 'listening_udp_ports', 'tcp_connections']
+
+with open('sample_data.csv', 'w') as writeFile:
+    writer = csv.writer(writeFile)
+    writer.writerows([row])
+writeFile.close()
 
 consumer = KafkaConsumer('defender_agent', bootstrap_servers=['localhost:9092'])
 print (consumer)
 for message in consumer:
-    m = json.loads((message.value).decode('utf-8'))
-    print (m)
+    data = json.loads((message.value).decode('utf-8'))
+    listdata = list()
+    listdata.append(data['metrics']['listening_tcp_ports']['total'])
+    listdata.append(data['metrics']['listening_udp_ports']['total'])
+    listdata.append(data['metrics']['tcp_connections']['established_connections']['total'])
+    print (listdata)
+    with open('sample_data.csv', 'a') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows([listdata])
+    writeFile.close()
